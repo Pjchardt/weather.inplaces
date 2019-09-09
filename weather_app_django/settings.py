b4058a02f545ba62fb0c9fce58449a3732158b80
Richard Hoagland <richiehoagland@gmail.com>
2019-09-09 00:25:44 -0400

    modifying setting.py to try and fix collect static error when deploying to heroku

--- weather_app_django/settings.py
+++ weather_app_django/settings.py
@@ -16,11 +16,18 @@ import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
-
 
 # SECURITY WARNING: keep the secret key used in production secret!
 if 'DARKSKY_KEY' not in os.environ:
@@ -120,12 +127,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
-
-STATIC_URL = '/static/'
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
