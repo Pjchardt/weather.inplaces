e93a9c905ff7bf74a702a5d7b0b8ab477e5ff200
Richard Hoagland <richiehoagland@gmail.com>
2019-09-09 02:26:07 -0400

    ahhhhh...still failing build when pushing to heroku

--- weather_app_django/settings.py
+++ weather_app_django/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import django_heroku
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -139,5 +138,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-# Activate Django-Heroku.
+# Configure Django App for Heroku.
+import django_heroku
 django_heroku.settings(locals())
